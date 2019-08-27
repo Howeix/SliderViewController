c9eb83d409b9dd3b5c7b074dd0188dc8f3c41d12
Howeix <495658492@qq.com>
2019-08-27 17:39:56 +0800

    testing

---
+++ SliderViewControllerDemo/SliderViewControllerDemo/Person.swift
@@ -0,0 +1,15 @@
+//
+//  Person.swift
+//  SliderViewControllerDemo
+//
+//  Created by Howeix on 2019/8/27.
+//  Copyright © 2019 Howeix. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+    
+    var name: String?
+    
+}
